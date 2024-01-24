1faaa19c4fdc1fe220d0bc7ac81783169697f985
Sinan Dönder <sinandondr@gmail.com>
2024-01-24 19:58:32 +0300

    "4.0.0 Airport, AirportType tested not working now."

--- src/main/java/com/flightSearchTest/flightsearchtest/Entities/Airport.java
+++ src/main/java/com/flightSearchTest/flightsearchtest/Entities/Airport.java
@@ -1,34 +1,19 @@
 package com.flightSearchTest.flightsearchtest.Entities;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.util.List;
+import lombok.*;
+import java.io.Serializable;
+
 
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 
-@Entity
-@Table(name = "airport")
-public class Airport {
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-    @Column(name = "city")
+public class Airport implements Serializable {
+    private Long id;
     private String city;
-
-    @OneToMany(mappedBy = "departureAirport")
-    private List<Flight> departure_flights;
-
-    @OneToMany(mappedBy = "arrivalAirport")
-    private List<Flight> arrival_flights;
 
 }
--- src/main/java/com/flightSearchTest/flightsearchtest/Entities/Flight.java
+++ src/main/java/com/flightSearchTest/flightsearchtest/Entities/Flight.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 import java.time.LocalDateTime;
 
@@ -20,6 +21,10 @@ public class Flight {
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    @Type(value = AirportType.class)
+    private Airport airport;
 
     @Column(name = "departure_airport")
     private String departureAirport;
